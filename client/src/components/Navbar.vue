f92a7105ddd16ea45f12c7e35de29dbd540a4489
hammond022 <75580640+hammond022@users.noreply.github.com>
2025-08-30 23:01:15 +0800

    fixed conflicts

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,7 @@
+<script>
+// import { RouterLink, useRoute } from "vue-router";
+</script>
+
+<template>
+  <nav><h1>Nav bar</h1></nav>
+</template>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>HOME</h1>
+  <div>
+    <label for="input-userId">Enter User ID HERE</label>
+    <input
+      type="text"
+      id="input-userId"
+      v-model="inputUserId"
+      placeholder="Enter user ID"
+    />
+  </div>
+</template>
